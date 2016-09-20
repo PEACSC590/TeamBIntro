6831a05ed658a0549b837005c4d547a16bcfba1e
Alec Sun <sundogx@gmail.com>
2016-09-20 15:32:04 -0400

    added input

---
+++ moo.java
@@ -0,0 +1,21 @@
+/* HelloWorld.java
+ */
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		int[][] numbers = new int[5][5];
+		for (int i=0; i< 5; i++) {
+			for (int j=0; j< 5; j++) {
+				numbers[i][j] = (int) Math.ceil(Math.random() * 200 - 100);
+		}
+
+		//saddle points
+
+		//output
+
+		//1 comment
+		
+
+	}
+}
